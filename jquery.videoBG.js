081882d36b38ea9b49ac230709edcffcdaa52579
kichik <kichik@gmail.com>
2017-04-04 22:48:02 -0700

    Make all video types optional

--- jquery.videoBG.js
+++ jquery.videoBG.js
@@ -223,19 +223,19 @@
     if ($.fn.videoBG.supportsVideo()) {
 
       // supports webm
-      if ($.fn.videoBG.supportType('webm')){
+      if ($.fn.videoBG.supportType('webm') && options.webm){
 
         // play webm
         $video.attr('src',options.webm);
       }
       // supports mp4
-      else if ($.fn.videoBG.supportType('mp4')) {
+      else if ($.fn.videoBG.supportType('mp4') && options.mp4) {
 
         // play mp4
         $video.attr('src',options.mp4);
       }
       // throw ogv at it then
-      else {
+      else if (options.ogv) {
 
         // play ogv
         $video.attr('src',options.ogv);
